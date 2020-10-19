35135cfe4cb7c0b59838d6e7968567e8efb3c355
EthanHoward <fishfingers342@gmail.com>
2020-10-19 19:39:38 +0100

    Removed.
    Removed all unnecesary tings.

--- README.md
+++
@@ -1,7 +0,0 @@
-*BUILT .EXE MOVED TO /build/dist/WibeScript.exe*
-
-![](gifs/wibescriptrgb.gif)
-
-![](gifs/3x.gif)
-
-'je vais tu va on the haters' - Tom
--- build/build.py
+++
@@ -1,8 +0,0 @@
-import os
-import time
-
-start_time = time.time()
-
-os.system('pyinstaller   --icon ./depend/icon.ico --console --onefile --noconfirm --name WibeScript ../src/ws.py')
-print('\nbuild.py\n | Build time:', time.time() - start_time, 's \n | Output: ./dist/WibeScript.exe\n\nWindow will close in 60s')
-time.sleep(60)
